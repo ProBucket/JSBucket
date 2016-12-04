1b2dd1c0b8fc63b3bd983ca5ddaf340b7f59f461
ProBucket <hasan.khan.pro@gmail.com>
2016-12-04 15:24:59 +0600

    Create gulpfile.js

---
+++ Gulp/gulpfile.js
@@ -0,0 +1,41 @@
+// npm install gulp-clean-css --save-dev
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+var install = require("gulp-install");
+
+// Install gulp packages  CMD: gulp script
+gulp.task('install', function(){
+	gulp.src('package.json')
+		.pipe(install());
+});
+
+gulp.task('scripts', function() {
+    gulp.src([
+			'js/scripts/vendors/jquery.min.js',
+			'js/lib/jquery-ui.js'
+		])
+        .pipe(concat('gradpak-admin-scripts.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('styles', function() {
+	gulp.src([
+			'css/fonts/ionicons/css/ionicons.min.css',
+			'css/lib/font-awesome.min.css'
+		])
+		.pipe(concat('gradpak-admin-styles.css'))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('build'))
+});
+
+
+// Default Task
+gulp.task('default', [           // CMD: gulp
+	'scripts',
+	'styles'
+]);
+
