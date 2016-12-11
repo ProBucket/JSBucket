ea1f015d3b34e93b235f2472319c89660da4e7eb
ProBucket <hasan.khan.pro@gmail.com>
2016-12-11 15:50:18 +0600

    Create method.js

---
+++ methods/method.js
@@ -0,0 +1,5 @@
+
+function showLoader(loader_dom){
+    var html_content  = '<div class="center-align blue-loader" style="margin-top: 200px;margin-left: 500px;"> <img  src=" /img/blue_loader.gif" alt="Loading" /> </div>';
+    $(loader_dom).html(html_content);
+}
