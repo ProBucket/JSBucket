86030a82afc1b5069980071cf9b8dddbe94da8bc
ProBucket <hasan.khan.pro@gmail.com>
2016-12-11 16:23:47 +0600

    Update method.js

--- methods/method.js
+++ methods/method.js
@@ -3,3 +3,8 @@ function showLoader(loader_dom){
     var html_content  = '<div class="center-align blue-loader" style="margin-top: 200px;margin-left: 500px;"> <img  src=" /img/blue_loader.gif" alt="Loading" /> </div>';
     $(loader_dom).html(html_content);
 }
+
+function scrollGoTo(classOrId){
+    $('html,body').animate({scrollTop: $(classOrId).offset().top}, 'slow');
+}
+
